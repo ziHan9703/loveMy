55874331ddd0fd8fd265502177b7d49fbbe2e6c0
zhuzihan <406854435@qq.com>
2025-02-17 20:34:47 +0800

    修改

--- vue.config.js
+++ vue.config.js
@@ -2,12 +2,12 @@
  * @Author: your name
  * @Date: 2025-02-17 17:07:26
  * @LastEditors: your name
- * @LastEditTime: 2025-02-17 20:24:35
+ * @LastEditTime: 2025-02-17 20:34:35
  * @Description: 
  * @FilePath: /love-demo/vue.config.js
  */
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: '/ziHan9703.github.io/'
+  publicPath: '/loveMy/'
 })
