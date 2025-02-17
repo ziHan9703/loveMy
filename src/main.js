6f1f6681a989d6bcd97161911dbefb70475c17be
zhuzihan <406854435@qq.com>
2025-02-17 20:44:38 +0800

    提交

--- src/main.js
+++ src/main.js
@@ -2,16 +2,13 @@
  * @Author: your name
  * @Date: 2025-02-17 17:07:26
  * @LastEditors: your name
- * @LastEditTime: 2025-02-17 17:53:53
+ * @LastEditTime: 2025-02-17 20:43:45
  * @Description: 
  * @FilePath: /love-demo/src/main.js
  */
 import Vue from 'vue'
 import App from './App.vue'
-import ElementUI from 'element-ui';
-import 'element-ui/lib/theme-chalk/index.css';
 Vue.config.productionTip = false
-Vue.use(ElementUI);
 new Vue({
   render: h => h(App),
 }).$mount('#app')
